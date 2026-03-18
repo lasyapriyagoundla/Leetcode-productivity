b2c2c88fa1a0f269a9cb9a8aca515d4a26e67712
lasyapriyagoundla <goundlalasyapriya@gmail.com>
2026-03-18 18:14:33 +0530

    Time: 1 ms (96.22%), Space: 42.5 MB (70.82%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -3,18 +3,21 @@ import java.util.*;
 class Solution {
     public boolean isPowerOfTwo(int n) {
       if(n<=0)
+      {
+
+      
       return false;
-        for(int i=0;i<31;i++)
-        {
-            int z=(int)Math.pow(2,i);
-            if(n==z)
-            {
-               return true;
-            }
-           
-             
-            
-        }
-    return false;
+      }
+      else
+      {
+    if((n & (n-1))==0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+      }
 }
 }
