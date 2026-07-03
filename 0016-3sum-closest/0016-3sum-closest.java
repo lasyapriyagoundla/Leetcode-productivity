1ec687201c6df7a0fc304610e84ccd922464466b
lasyapriyagoundla <goundlalasyapriya@gmail.com>
2026-07-03 20:35:02 +0530

    Time: 17 ms (78.5%), Space: 45.6 MB (30.38%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -3,7 +3,7 @@ class Solution {
         int n=nums.length;
           Arrays.sort(nums);
           int cl=nums[0]+nums[1]+nums[2];
-        for(int i=0;i<n;i++)
+        for(int i=0;i<n-2;i++)
         {
             int l=i+1,r=n-1;
             while(l<r)
