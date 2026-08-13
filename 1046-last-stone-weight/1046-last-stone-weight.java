12fa9db56bf2f1ada680e7f1de63b85d7bde4bc8
lasyapriyagoundla <goundlalasyapriya@gmail.com>
2026-08-13 20:37:11 +0530

    Time: 3 ms (5.05%), Space: 42.5 MB (96.99%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> pq=new PriorityQueue<>(Collections.reverseOrder());
+        for(int n:stones)
+        {
+            pq.add(n);
+        }
+        while(pq.size()>1)
+        {
+            int x=pq.poll();
+            int y=pq.poll();
+            if(x!=y)
+            {
+                pq.add(Math.abs(y-x));
+            }
+
+        }
+        if(pq.isEmpty())
+        {
+            return 0;
+        }
+       return pq.peek(); 
+    }
+}
